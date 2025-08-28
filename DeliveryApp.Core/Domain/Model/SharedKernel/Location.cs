409f83ed854690a29e9a85fa061b8a5ad8d8d6a9
Dimonoid2013@yandex.ru <Dimonoid2013@yandex.ru>
2025-08-28 13:15:29 +0300

    feat(core):Add_SharedKernel_and_unittests

---
+++ DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
@@ -0,0 +1,63 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Core.Domain.Model.SharedKernel
+{
+    public class Location : ValueObject
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        [ExcludeFromCodeCoverage]
+        private Location() { }
+
+        private Location(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Result<Location, Error> Create(int x, int y)
+        {
+            if (x < 1 || x > 10)
+            {
+                return GeneralErrors.ValueIsRequired(nameof(x));
+            }
+
+            if (y < 1 || y > 10)
+            {
+                return GeneralErrors.ValueIsRequired(nameof(y));
+            }
+
+            return new Location(x, y);
+        }
+
+        public static Location CreateRandom()
+        {
+            var randomValueX = Random.Shared.Next(1, 11);
+            var randomValueY = Random.Shared.Next(1, 11);
+
+            return new Location(randomValueX, randomValueY);
+        }
+
+        public Result<int, Error> DistanceTo(Location target)
+        {
+            if (target == null)
+            {
+                return GeneralErrors.NotFound();
+            }
+
+            var distance = Math.Abs(X - target.X) + Math.Abs(Y - target.Y);
+
+            return distance;
+        }
+
+        [ExcludeFromCodeCoverage]
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,109 @@
+﻿using DeliveryApp.Core.Domain.Model.SharedKernel;
+using FluentAssertions;
+using Primitives;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.SharedKernel
+{
+    public class LocationShould
+    {
+        [Fact]
+        public void BeCorrectWhenIsParamsIsCorrectOnCreated()
+        {
+            //Arrange
+
+            //Act
+            var location = Location.Create(5, 5);
+
+            //Assert
+            location.IsSuccess.Should().BeTrue();
+            location.Value.X.Should().Be(5);
+            location.Value.Y.Should().Be(5);
+        }
+
+        [Theory]
+        [InlineData(0, 11)]
+        [InlineData(11, 0)]
+        public void ReturnErrorWhenIsParamsIsCorrectOnCreated(int x, int y)
+        {
+            //Arrange
+
+            //Act
+            var location = Location.Create(x, y);
+
+            //Assert
+            location.IsSuccess.Should().BeFalse();
+            location.Error.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void BeEqualWhenAllPropertiesIsEqual()
+        {
+            //Arrange
+            var firstLocation = Location.Create(5, 5).Value;
+            var secondLocation = Location.Create(5, 5).Value;
+
+            //Act
+            var location = firstLocation == secondLocation;
+
+            //Assert
+            location.Should().BeTrue();
+        }
+
+        [Fact]
+        public void BeNotEqualWhenAllPropertiesIsEqual()
+        {
+            //Arrange
+            var firstLocation = Location.Create(5, 1).Value;
+            var secondLocation = Location.Create(1, 5).Value;
+
+            //Act
+            var location = firstLocation == secondLocation;
+
+            //Assert
+            location.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ReturnDistanceWhenIsParamsIsCorrectOnDistanceTo()
+        {
+            //Arrange
+            var locationCurier = Location.Create(1, 1).Value;
+            var locationTarget = Location.Create(3, 3).Value;
+
+            //Act
+            var result = locationCurier.DistanceTo(locationTarget);
+
+            //Assert
+            result.Value.Should().Be(4);
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ReturnErrorWhenTargetNullOnDistanceTo()
+        {
+            //Arrange
+            var locationCurier = Location.Create(1, 1).Value;
+
+            //Act
+            var result = locationCurier.DistanceTo(null);
+
+            //Assert
+            result.IsFailure.Should().BeTrue();
+            result.Error.Should().BeEquivalentTo(GeneralErrors.NotFound());
+        }
+
+        [Fact]
+        public void BeCorrectWhenIsParamsIsCorrectOnCreatedRandom()
+        {
+            //Arrange
+
+            //Act
+            var location = Location.CreateRandom();
+
+            //Assert
+            location.X.Should().BeInRange(1, 10);
+            location.X.Should().BeInRange(1, 10);
+        }
+    }
+}
