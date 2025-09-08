105c928369644c8f9802fd0f678ed1512da41458
Dimonoid2013@yandex.ru <Dimonoid2013@yandex.ru>
2025-09-08 09:56:46 +0300

    feat(domain):Add_DomainService

---
+++ DeliveryApp.Core/Domain/Services/DispatchCourier/DispatchService.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Services.DispatchCourier
+{
+    public class DispatchService : IDispatchService
+    {
+        public Result<Courier, Error> Scoring(Order order, List<Courier> freeCouriers)
+        {
+            if (order == null)
+            {
+                return GeneralErrors.ValueIsRequired(nameof(order));
+            }
+
+            if (freeCouriers == null)
+            {
+                return GeneralErrors.ValueIsRequired(nameof(freeCouriers));
+            }
+
+            var couriers = freeCouriers.Where(x => x.CanTakeOrder(order).IsSuccess);
+
+            if (!couriers.Any())
+            {
+                return GeneralErrors.NotFound();
+            }
+            var courier = couriers.OrderBy(x => x.CalculateTimeToLocation(order.Location)).ToList()[0];
+            courier.TakeOrder(order);
+
+            return courier;
+        }
+    }
+}
