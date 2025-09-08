356614e86c1fd013eef1d1946dc12d62ccd1a437
Dimonoid2013@yandex.ru <Dimonoid2013@yandex.ru>
2025-09-08 18:39:57 +0300

    feat(infrastructure):Add_CourierRepository

--- DeliveryApp.Core/Ports/ICourierRepository.cs
+++ DeliveryApp.Core/Ports/ICourierRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace DeliveryApp.Core.Ports
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+
+namespace DeliveryApp.Core.Ports
 {
+    /// <summary>
+    /// Порт для работы с курьерами
+    /// </summary>
     public interface ICourierRepository
     {
+        /// <summary>
+        /// Добавление курьера
+        /// </summary>
+        /// <param name="courier">Курьер</param>
+        /// <returns></returns>
+        Task AddAsync(Courier courier);
+
+        /// <summary>
+        /// Обновление данных курьера
+        /// </summary>
+        /// <param name="courier">Курьер</param>
+        /// <returns></returns>
+        void Update(Courier courier);
+
+        /// <summary>
+        /// Получение курьера по Id
+        /// </summary>
+        /// <param name="courierId"></param>
+        /// <returns></returns>
+        Task<Courier> GetByIdAsync(Guid courierId);
+
+        /// <summary>
+        /// Получение всех свободных курьеров
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Courier>> GetAllFreeAsync();
     }
 }
---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/CourierRepository.cs
@@ -0,0 +1,51 @@
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories
+{
+    public class CourierRepository : ICourierRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CourierRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext; throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task AddAsync(Courier courier)
+        {
+            if (courier == null)
+            {
+                throw new ArgumentNullException("Courier is Null");
+            }
+            await _dbContext.Couriers.AddAsync(courier);
+        }
+
+        public async Task<List<Courier>> GetAllFreeAsync()
+        {
+            var freeCouriers = await _dbContext.Couriers.Where(x => x.StoragePlaces.All(sp => sp.OrderId == null))
+                                                        .ToListAsync();
+            if (freeCouriers == null)
+            {
+                throw new ArgumentNullException("FreeCouriers Not Found");
+            }
+            return freeCouriers;
+        }
+
+        public async Task<Courier> GetByIdAsync(Guid courierId)
+        {
+            return await _dbContext.Couriers.FindAsync(courierId)
+                         ?? throw new ArgumentNullException($"Courier c Id {courierId} Not Found"); ;
+        }
+
+        public void Update(Courier courier)
+        {
+            if (courier == null)
+            {
+                throw new ArgumentNullException("Courier is Null");
+            }
+            _dbContext.Couriers.Update(courier);
+        }
+    }
+}
