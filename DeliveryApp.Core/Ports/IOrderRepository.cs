8311e567917f3ee381f20c88edd0ac1df336b4f5
Dimonoid2013@yandex.ru <Dimonoid2013@yandex.ru>
2025-09-08 15:28:57 +0300

    feat(core):Add_IOrderRepository_Ports

---
+++ DeliveryApp.Core/Ports/IOrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using DeliveryApp.Core.Domain.Model.OrderAggregate;
+
+namespace DeliveryApp.Core.Ports
+{
+    /// <summary>
+    /// Порт взаимодействия с заказами
+    /// </summary>
+    public interface IOrderRepository
+    {
+        /// <summary>
+        /// Добавление заказа
+        /// </summary>
+        /// <param name="order">Заказ</param>
+        /// <returns></returns>
+        Task AddAsync(Order order);
+
+        /// <summary>
+        /// Обновление заказа
+        /// </summary>
+        /// <param name="order">Закаp</param>
+        /// <returns></returns>
+        Task UpdateAsync(Order order);
+
+        /// <summary>
+        /// Получение закааза по Id
+        /// </summary>
+        /// <param name="orderId">Id заказа</param>
+        /// <returns></returns>
+        Task<Order> GetByIdAsync(Guid orderId);
+
+        /// <summary>
+        /// Получение любого заказа со статусом Created
+        /// </summary>
+        /// <returns></returns>
+        Task<Order> GetCreated();
+
+        /// <summary>
+        /// Получение списка назначенных заказов
+        /// </summary>
+        /// <returns></returns>
+        Task<IQueryable<Order>> GetAllAssignedAsync();
+    }
+}
